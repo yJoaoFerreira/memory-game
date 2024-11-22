e071b24468f261beed57d82cff3a0e67010836f9
Joao Ferreira <yferreirajoao@gmail.com>
2024-11-21 21:12:51 -0300

    random emojis js

---
+++ src/scripts/engine.js
@@ -0,0 +1,21 @@
+const emojis = [
+    "🍏", "🍏",
+    "🍎", "🍎",
+    "🍐", "🍐",
+    "🍊", "🍊",
+    "🍋", "🍋",
+    "🍌", "🍌",
+    "🍉", "🍉",
+    "🍇", "🍇",
+];
+
+let OpenCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for (let i = 0; i < emojis.length; i++){
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box);
+}
