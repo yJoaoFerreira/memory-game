817af7b91b4aca7f7935a26992dfd5782e4b401b
Joao Ferreira <yferreirajoao@gmail.com>
2024-11-21 21:46:52 -0300

    finish project

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -9,43 +9,43 @@ const emojis = [
     "🍇", "🍇",
 ];
 
-let OpenCards = [];
+let openCards = [];
 
 let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
 for (let i = 0; i < emojis.length; i++) {
-    let box = document.createElement("div");
-    box.className = "item";
-    box.innerHTML = shuffleEmojis[i];
-    box.onclick = handleClick;
-    document.querySelector(".game").appendChild(box);
+  let box = document.createElement("div");
+  box.className = "item";
+  box.innerHTML = shuffleEmojis[i];
+  box.onclick = handleClick;
+  document.querySelector(".game").appendChild(box);
 };
 
-function handleClick () {
-    if (OpenCards.length < 2) {
-        this.classList.add("boxOpen");
-        OpenCards.push(this);
-    };
+function handleClick() {
+  if (openCards.length < 2) {
+    this.classList.add("boxOpen");
+    openCards.push(this);
+  };
 
-    if (OpenCards.length == 2) {
-        setTimeout(checkMatch, 500);
-    };
+  if (openCards.length == 2) {
+    setTimeout(checkMatch, 500);
+  };
 
-    console.log(OpenCards);
+  console.log(openCards);
 };
 
-function checkMatch () {
-    if (OpenCards[0].innerHTML === OpenCards[1].innerHTML) {
-        OpenCards[0].classList.add("boxMatch");
-        OpenCards[1].classList.add("boxMatch");
-    } else {
-        OpenCards[0].classList.remove("boxMatch");
-        OpenCards[1].classList.remove("boxMatch");
-    };
+function checkMatch() {
+  if (openCards[0].innerHTML === openCards[1].innerHTML) {
+    openCards[0].classList.add("boxMatch");
+    openCards[1].classList.add("boxMatch");
+  } else {
+    openCards[0].classList.remove("boxOpen");
+    openCards[1].classList.remove("boxOpen");
+  };
 
-    OpenCards = [];
+  openCards = [];
 
-    if (document.querySelectorAll(".boxMatch").length == emojis.length) {
-        alert("Você venceu!");
-    };
-};
+  if (document.querySelectorAll(".boxMatch").length === emojis.length) {
+    alert("Você venceu!");
+  };
+}
