b3f435e39c519cd002210f9cfbe1fa7052171fae
Joao Ferreira <yferreirajoao@gmail.com>
2024-11-21 21:28:46 -0300

    Function virar as cartas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -13,9 +13,25 @@ let OpenCards = [];
 
 let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
-for (let i = 0; i < emojis.length; i++){
+for (let i = 0; i < emojis.length; i++) {
     let box = document.createElement("div");
     box.className = "item";
     box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
     document.querySelector(".game").appendChild(box);
 }
+
+function handleClick () {
+    if(OpenCards.length < 2) {
+        this.classList.add("boxOpen");
+        OpenCards.push(this);
+    };
+
+    if(OpenCards.length == 2) {
+        setTimeout(checkMatch, 500);
+    }
+};
+
+function checkMatch () {
+    // 
+};
